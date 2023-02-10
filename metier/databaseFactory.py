2561ef2ebf9bfa45a929f6c908f776ace94cf49a
markomartic <marticmarkos86@gmail.com>
2023-02-10 11:37:23 +0100

    typo

---
+++ metier/databaseFactory.py
@@ -0,0 +1,95 @@
+import abc
+import psycopg2
+import metier
+
+class AbstractDatabase(abc.ABC):
+    
+    @abc.abstractmethod
+    def get_body_type(self):
+        pass
+
+    @abc.abstractmethod
+    def connexion(self):
+        pass
+
+    @abc.abstractmethod
+    def add_client(self, client: metier.Client):
+        pass
+
+    @abc.abstractmethod
+    def get_all_clients(self):
+        pass
+
+    @abc.abstractmethod
+    def get_client(self, id):
+        pass
+
+
+class PostgresDB(AbstractDatabase):
+
+    def __init__(self):
+        self.connector = None
+        self.cursor = None
+        self.connexion()
+        self.create_client_table_if_not_exist()
+        self.connected = False
+
+    def connexion(self):
+        try:
+            self.connector = psycopg2.connect(
+                host="127.0.0.1",
+                database="my_company_db",
+                user="postgres",
+                password="password",
+                port="5432"
+            )
+            self.cursor = self.connector.cursor()
+            self.connected = True
+        except:
+            print("connection impossible")
+
+    def add_client(self, newClient: metier.Client):
+        self.cursor.execute("INSERT INTO clients(firstName, lastName, emailId) values(%s,%s,%s)",(newClient.firstName, newClient.lastName, newClient.emailId))
+        self.connector.commit()
+
+    def get_all_clients(self):
+        self.cursor.execute("SELECT * FROM clients")
+        return self.cursor.fetchall()
+
+    def get_client(self, id):
+        self.cursor.execute("SELECT * FROM clients WHERE id=%s", str(id))
+        return self.cursor.fetchall()
+
+    def create_client_table_if_not_exist(self):
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS clients (id serial NOT NULL PRIMARY KEY, firstName varchar(255) NOT NULL, lastName varchar(255) NOT NULL, emailId varchar(255) NOT NULL);')
+        self.connector.commit()
+
+    def get_body_type(self):
+        return "test"
+    
+class JsonDB(AbstractDatabase):
+    
+    def __init__(self):
+        pass
+        
+    def get_body_type(self):
+        pass
+
+class DatabaseFactory():
+    
+    def build_db(plan):
+        try:
+            if plan == "postgres":
+                return PostgresDB()
+            elif plan == "json":
+                return JsonDB()
+            raise AssertionError("Type is not valid.")
+        except AssertionError as e:
+            print(e)
+        except NoneType as e:
+            print(e)
+
+if(__name__ == "__main__"):
+    db = DatabaseFactory.build_db("postgres")
+    newone = metier.Client("fn", "ln", "email@email.com")
+    db.add_client(newone)
